2e96ee0b70ee44d5857759ea27721196815a39ad
Nirmal <138759440+Nirmalll@users.noreply.github.com>
2023-07-27 16:42:38 +0530

    Add files via upload

---
+++ ArrayCollectin.java
@@ -0,0 +1,11 @@
+package arraylist;
+import java.util.List;
+
+public class ArrayCollectin {
+
+	public static void main(String[] args) {
+		List values= List.of("a",'a',8,8.0);
+		System.out.println(values.get(2));
+	}
+
+}
